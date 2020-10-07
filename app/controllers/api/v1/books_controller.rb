660a1baa89b7ae55267853a293d3846621742cf1
Taiji Namba <namba9499@gmail.com>
2020-10-07 21:17:20 +0900

    apiフォルダ削除, searchメソッドで最初の10冊取得

--- app/controllers/api/v1/books_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::BooksController < ApplicationController
-  
-  def search
-  books = RakutenWebService::Books::Book.search(keyword: "Ruby")
-  items.page(3).all do |item|
-  puts item.name
-  end
-  
-end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
   def search
-    
+  # books = RakutenWebService::Books::Book.search(keyword: "Ruby")
+  # items.page(3).all do |item|
+  # puts item.name
+
+    books = RakutenWebService::Books::Book.search(keyword: "Ruby")
+    books.first(10).each do |book|
+      # if book["availability"] == 1 #在庫があるか
+
+      #取得したデータを取得
+    # puts book.title
+    end
   end
 end
