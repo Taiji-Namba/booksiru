4591d74db7aa24f1c66ddee0c026b7ad2804bc88
Taiji Namba <namba9499@gmail.com>
2020-10-09 23:04:34 +0900

    検索結果が反映されない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,6 @@ class BooksController < ApplicationController
   def home
     books = RakutenWebService::Books::Book.search(title: "Ruby")
     books.first(10).each do |book|
-      # if book["availability"] == 1 #在庫があるか
 
       #取得したデータを取得
       puts book.title
@@ -16,13 +15,14 @@ class BooksController < ApplicationController
   end
 
   def search
-      @books = RakutenWebService::Books::Book.search(params[:book])
+      if params[:keyword]
+      @books = RakutenWebService::Books::Book.search(keyword: params[:keyword])
     # books = RakutenWebService::Books::Book.search(keyword: "Ruby")
     # items.page(3).all do |item|
     # puts item.name
 
     # @books = Book.search(params[:book])
 
-    p @books
+    end
   end
 end
