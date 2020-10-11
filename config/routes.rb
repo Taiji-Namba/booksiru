1d17572622d50ed29c30d9d23c2c81cd12c7dbec
Taiji Namba <namba9499@gmail.com>
2020-10-11 15:18:22 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "books#home"
+  devise_scope :user do 
+    post "users/guest_sign_in", to: "users/sessions#new_guest"
+  end
+
+  root "books#home"
   resources :books do
     get "search", on: :collection
   end
