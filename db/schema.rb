c8129ce0f8bb7e0f92195617e089c424269292ea
Taiji Namba <namba9499@gmail.com>
2020-10-17 17:25:12 +0900

    Genreモデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_120852) do
+ActiveRecord::Schema.define(version: 2020_10_17_082131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "genre"
     t.integer "booksgenreid"
     t.datetime "created_at", precision: 6, null: false
