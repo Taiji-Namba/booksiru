fa25252fa608a8c89491be2e12a70e85c78e04e7
Taiji Namba <namba9499@gmail.com>
2020-10-18 21:54:32 +0900

    genre1にすべてのジャンルを設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-genre1 = Genre.create!(genre: "コミック", booksgenreid: "001001")
-genre2 = Genre.create!(genre: "小説・エッセイ", booksgenreid: "001004")
-genre3 = Genre.create!(genre: "ライトノベル", booksgenreid: "001017")
+genre1 = Genre.create!(genre: "すべてのジャンル", booksgenreid: "001")
+genre2 = Genre.create!(genre: "コミック", booksgenreid: "001001")
+genre3 = Genre.create!(genre: "小説・エッセイ", booksgenreid: "001004")
+genre4 = Genre.create!(genre: "ライトノベル", booksgenreid: "001017")
