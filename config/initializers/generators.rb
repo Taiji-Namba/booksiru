67a6511902a94f9dedf710a416ab8874b834641f
Taiji Namba <namba9499@gmail.com>
2020-10-26 22:12:24 +0900

    rspec導入

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,4 +2,11 @@ Rails.application.config.generators do |g|
   g.assets false
   g.helper false
   g.skip_routes true
+  g.test_framework :rspec,
+                    fixtures: true,
+                    view_specs: false,
+                    routing_specs: false,
+                    helper_specs: false,
+                    controller_spec: false,
+                    request_specs: true
 end
