e3e909ee8f9b354d637d88adf16f369af685b1b1
Taiji Namba <namba9499@gmail.com>
2020-11-03 18:32:31 +0900

    create,destroyにpath通し完了、author_favoriteに一意性制約

---
+++ app/controllers/author_favorites_controller.rb
@@ -0,0 +1,24 @@
+class AuthorFavoritesController < ApplicationController
+
+  def index
+    @author_favorites = AuthorFavorite.order(:id).includes(:user)
+  end
+
+  def create
+    author_favorite = current_user.author_favorites.create!(author_name: params[:author_name])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    author_favorite = current_user.author_favorites.find_by(author_name: params[:author_name])
+    author_favorite.destroy!
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  # def author_name
+  #   params.require(:author_favorite).permit(:author_name)
+  # end
+    
+end
--- app/models/author_favorite.rb
+++ app/models/author_favorite.rb
@@ -1,4 +1,7 @@
 class AuthorFavorite < ApplicationRecord
   belongs_to :user
-  validates :author_name, presence: true
+  validates :author_name, presence: true, uniqueness: {
+    scope: :user_id,
+    message: "はすでにお気に入り登録されています"
+  }
 end
