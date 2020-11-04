0812461cf917b2ae29e068048c97055d5ad7ff6f
Taiji Namba <namba9499@gmail.com>
2020-11-04 23:55:12 +0900

    ajax実装一旦中止・destroyアクションのルーティングを設定試み

--- app/controllers/author_favorites_controller.rb
+++ app/controllers/author_favorites_controller.rb
@@ -6,13 +6,13 @@ class AuthorFavoritesController < ApplicationController
 
   def create
     author_favorite = current_user.author_favorites.create!(author_name: params[:author_name])
-    # redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     author_favorite = current_user.author_favorites.find_by(author_name: params[:author_name])
     author_favorite.destroy!
-    # redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
 
   # private
