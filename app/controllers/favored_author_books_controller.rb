bfc8495159d344d840dd34ee308601dc582a7214
Taiji Namba <namba9499@gmail.com>
2020-11-09 23:17:03 +0900

    Revert "favored_author_books#createの記述方法模索中"
    This reverts commit fc7fddb5fa36337f8b2f6a6e3e540c0fdf514163.

--- app/controllers/favored_author_books_controller.rb
+++ app/controllers/favored_author_books_controller.rb
@@ -1,19 +1,6 @@
 class FavoredAuthorBooksController < ApplicationController
   def create
-    
-    favored_author_books = RakutenWebService::Books::Book.search(
-      author: params[:author_name] # このauthor_nameはauthor_favorite_idごとのauthor_name。わからないがparamsで受け取れる？
-    )
 
-    favored_author_books.each do |book|
-      "author_favorite_idがauthor_favorites#createで登録されたauthor_favorite.idのAuthorFavoriteインスタンス".favored_author_books.create!(
-        isbn: book.isbn,
-        title: book.title,
-        略
-      )
-      favored_author_book = FavoredAuthorBook.find(params[:favored_author_book_id])
-    end
-  
   end
 
   def show
