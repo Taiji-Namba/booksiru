98c02b1198624a89c5e2f54fe2fc066acb1ece6e
Taiji Namba <namba9499@gmail.com>
2020-11-11 21:15:32 +0900

    著者お気に入りされているか否かでビューを条件分岐

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,9 @@ class Author < ApplicationRecord
   has_many :author_favorites, dependent: :destroy
   validates :author_name, presence: true
   accepts_nested_attributes_for :author_favorites
+
+  def self.registered(book)
+    find_by(author_name: book.author.gsub(" ","").gsub("　",""))
+  end
+  
 end
