5ff567876f704071327937d0a2176c7bd8586df3
Taiji Namba <namba9499@gmail.com>
2020-11-15 15:43:49 +0900

    お気に入り解除ボタンを押すとauthor_favoritesのデータが削除

--- app/controllers/author_favorites_controller.rb
+++ app/controllers/author_favorites_controller.rb
@@ -4,21 +4,9 @@ class AuthorFavoritesController < ApplicationController
     @author_favorites = AuthorFavorite.order(:id).includes(:user)
   end
 
-  def create
-    current_user.author_favorites.create!(author_id: author.id)
-    redirect_back(fallback_location: root_path)
-  end
-
   def destroy
-    author_favorite = current_user.author_favorites.find_by(author_name: params[:author_name])
-    author_favorite.destroy!
+    current_user.author_favorites.find_by(author_id: params[:author_id]).destroy!
     redirect_back(fallback_location: root_path)
   end
-
-  # private
-
-  # def author_name
-  #   params.require(:author_favorite).permit(:author_name)
-  # end
     
 end
--- app/models/author_favorite.rb
+++ app/models/author_favorite.rb
@@ -6,8 +6,8 @@ class AuthorFavorite < ApplicationRecord
     message: "はすでにお気に入り登録されています"
   }
 
-  def self.favored(author, user)
-    find_by(author_id: author.id, user_id: user.id)
+  def self.favored_by?(author, user)
+    find_by(author_id: author.id, user_id: user.id).present?
   end
 
 end
