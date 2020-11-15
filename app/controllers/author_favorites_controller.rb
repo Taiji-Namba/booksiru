80d0da419b9dd28ede001fab5519d6298129863e
Taiji Namba <namba9499@gmail.com>
2020-11-15 17:57:37 +0900

    他のユーザーが同じ著者をお気に入り登録している場合はauthor_favoriteのデータのみ削除

--- app/controllers/author_favorites_controller.rb
+++ app/controllers/author_favorites_controller.rb
@@ -6,6 +6,12 @@ class AuthorFavoritesController < ApplicationController
 
   def destroy
     current_user.author_favorites.find_by(author_id: params[:author_id]).destroy!
+    
+    if AuthorFavorite.find_by(author_id: params[:author_id]).nil?
+      author = Author.find_by(id: params[:author_id])
+      author.destroy!
+    end
+    
     redirect_back(fallback_location: root_path)
   end
     
