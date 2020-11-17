c8ae1e4a87d517512da7e6d9f3f20db2ba4c9b2d
Taiji Namba <namba9499@gmail.com>
2020-11-17 22:18:24 +0900

    未発売の本のみfavored_author_booksテーブル登録に必要なパラメーターを渡すよう条件分岐

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,14 +2,37 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.find_or_create_by!(author_params)
+
+    # ログインユーザーのお気に入りに登録
     current_user.author_favorites.create!(author_id: author.id)
+    
+    # 登録した著者の未発売の書籍をFavoredAuthorBookに登録
+    # favored_author_book = FavoredAuthorBook.find_or_create_by!(
+    #   author_id: author.id,
+    #   author_params
+    # )
+    
     redirect_back(fallback_location: root_path)
+
   end
 
   private
 
     def author_params
       params.require(:author).permit(:author_name)
+    end
+
+    def favored_author_book_params
+      params.require(:favored_author_books).permit(
+        :isbn,
+        :title,
+        :sales_date,
+        :image_url,
+        :item_url,
+        :item_price,
+        :booksgenreid,
+        :size
+      )
     end
 
 end
