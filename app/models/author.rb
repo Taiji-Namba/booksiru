ba25337f2962cb5f30295ec8c19496c779eb87ac
Taiji Namba <namba9499@gmail.com>
2020-11-17 23:30:31 +0900

    gsubからdelelteに修正

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,7 @@ class Author < ApplicationRecord
   validates :author_name, presence: true, uniqueness: {message: "はすでにお気に入り登録されています"}
 
   def self.registered(book)
-    find_or_initialize_by(author_name: book.author.gsub(" ","").gsub("　",""))
+    find_or_initialize_by(author_name: book.author.delete("/ |　/"))
   end
   
 end
