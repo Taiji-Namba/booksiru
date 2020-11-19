c82ea33a6c95413cee8e8ea8e71366169273aed8
Taiji Namba <namba9499@gmail.com>
2020-11-19 22:14:36 +0900

    未発売の本だけfavored_author_booksテーブルに登録

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,20 @@
 class AuthorsController < ApplicationController
 
   def create
+    # ボタンを押すと著者(author)がauthorsテーブルに登録される。すでに登録されている場合はauthorを取得するだけ
     author = Author.find_or_create_by!(author_params)
 
     # ログインユーザーのお気に入りに登録
     current_user.author_favorites.create!(author_id: author.id)
     
+    #お気に入り登録した著者の本を取得
     to_be_favored_author_books = RakutenWebService::Books::Book.search(
     author: author.author_name,
     sorts: "-releaseDate",
     )
 
-    favored_author_books = to_be_favored_author_books.map do |b|
+    # 未発売の本だけfavored_author_booksテーブルに各種情報を登録
+    favored_author_books = to_be_favored_author_books.select{|tob| tob.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}.map do |b|
       FavoredAuthorBook.new(
         author_name: author.author_name,
         isbn: b.isbn,
@@ -24,7 +27,6 @@ class AuthorsController < ApplicationController
         size: b.size
       )
     end
-
     FavoredAuthorBook.import favored_author_books
         
     redirect_back(fallback_location: root_path)
