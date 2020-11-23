1697084dca9a2f3f5b312880ea5d93fc4151ee11
Taiji Namba <namba9499@gmail.com>
2020-11-23 19:49:37 +0900

    schedule.rbの設定変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,8 @@
-every 1.day, at: "5:10 pm" do
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+rails_env = ENV["RAILS_ENV"] || :development
+set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
+
+every 1.minute do
   rake "notice_book:create_notice"
 end
-
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
