8151ab2347bd67f8f0c240f1ff457e2bb703fccb
Taiji Namba <namba9499@gmail.com>
2020-11-28 18:23:21 +0900

    新規ユーザー登録完了メールのメイラー記述

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,9 @@
 class NotificationMailer < ApplicationMailer
+  default from: "booksiru164@gmail.com"
+
+  def welcome_email
+    @user = params[:user]
+    @url = "https://booksiru.herokuapp.com"
+    mail(subject: "新規登録ありがとうございます！", to: @user.email)
+  end
 end
