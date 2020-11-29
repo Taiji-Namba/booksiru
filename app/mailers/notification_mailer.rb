e21a911d0d5569e24546c9adb30d85d23c02911d
Taiji Namba <namba9499@gmail.com>
2020-11-29 15:05:33 +0900

    notification_mailer.rbに通知メールのメソッド記載

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+
+  def send_notification_email
+    @user = user
+    @favored_author_book = favored_author_book
+    mail(subject: "#【{@favored_author_book.title}】があと3日で発売です", to: @user.email)
+  end
 end
