eb33333104056d8fb91edcbb2c6cf2e927d788b3
Taiji Namba <namba9499@gmail.com>
2020-12-15 20:08:01 +0900

    FavoredBookのカラムを定義

--- db/migrate/20201215110424_create_favored_books.rb
+++ db/migrate/20201215110424_create_favored_books.rb
@@ -1,6 +1,16 @@
 class CreateFavoredBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :favored_books do |t|
+      t.string :author_name
+      t.string :isbn
+      t.string :title
+      t.string :title_kana
+      t.string :sales_date
+      t.integer :days_to_release
+      t.string :item_url
+      t.string :item_price
+      t.string :publisher_name
+      t.string :size
 
       t.timestamps
     end
