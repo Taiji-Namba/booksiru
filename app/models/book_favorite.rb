cdd9f6a6c80761551239d14785c55d6ee8459255
Taiji Namba <namba9499@gmail.com>
2020-12-15 21:14:13 +0900

    favored_books登録済みか否かの判定メソッド定義

--- app/models/book_favorite.rb
+++ app/models/book_favorite.rb
@@ -5,4 +5,9 @@ class BookFavorite < ApplicationRecord
     scope: :favored_book_id,
     message: "は同じ系列の本に2回以上いいねはできません"
   }
+
+  def self.favored_by?(favored_book, user)
+    find_by(favored_book_id: favored_book.id, user_id: user.id).present?
+  end
+  
 end
