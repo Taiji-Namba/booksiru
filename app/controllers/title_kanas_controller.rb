6a08e206c3e09812ef4f9b6e81cec4fb9d2f3739
Taiji Namba <namba9499@gmail.com>
2020-12-15 23:13:51 +0900

    title_kanasのストロングパラメーターの部分修正

--- app/controllers/title_kanas_controller.rb
+++ app/controllers/title_kanas_controller.rb
@@ -1,7 +1,7 @@
 class TitleKanasController < ApplicationController
   def create
     # ボタンを押すと作品名がfavored_booksテーブルに登録される。すでに登録されている場合は取得するだけ
-    favored_book = FavoredBook.find_or_create_by!(favored_book_params)
+    favored_book = FavoredBook.find_or_create_by!(title_kana_params)
 
     # ログインユーザーのお気に入り作品に登録
     book_favorite = current_user.book_favorites.create!(favored_book_id: favored_book.id)
