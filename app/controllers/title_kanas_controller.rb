769d3b24855680cd567aca6b3a252d7c7e2322f0
Taiji Namba <namba9499@gmail.com>
2020-12-15 23:33:35 +0900

    お気に入り作品カナ登録実装

--- app/controllers/title_kanas_controller.rb
+++ app/controllers/title_kanas_controller.rb
@@ -3,11 +3,13 @@ class TitleKanasController < ApplicationController
     # ボタンを押すとその作品のカナがtitle_kanaテーブルに登録される。すでに登録されている場合は取得するだけ
     title_kana = TitleKana.find_or_create_by!(title_kana_params)
 
-    # ログインユーザーのお気に入り作品に登録
-  #   book_favorite = current_user.book_favorites.create!(title_kana_id: title_kana.id)
-  # end
+    # ログインユーザーのお気に入り作品カナに登録
+    book_favorite = current_user.book_favorites.create!(title_kana_id: title_kana.id)
 
-private
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
 
   def title_kana_params
     params.require(:title_kana).permit(:title_kana)
--- app/models/title_kana.rb
+++ app/models/title_kana.rb
@@ -2,4 +2,9 @@ class TitleKana < ApplicationRecord
   has_many :book_favorites, dependent: :destroy
   has_many :favored_user, through: :book_favorites, source: :user
   validates :title_kana, presence: true, uniqueness: {message: "はすでにお気に入り登録されています"}
+
+  def self.registered(book)
+    find_or_initialize_by(title_kana: book.title_kana)
+  end
+
 end
