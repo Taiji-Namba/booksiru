d11200172449948fe6a7c6c221ff23d1f4213e2c
Taiji Namba <namba9499@gmail.com>
2020-12-15 23:39:10 +0900

    favored_booksテーブルの登録実装

--- app/controllers/title_kanas_controller.rb
+++ app/controllers/title_kanas_controller.rb
@@ -6,6 +6,37 @@ class TitleKanasController < ApplicationController
     # ログインユーザーのお気に入り作品カナに登録
     book_favorite = current_user.book_favorites.create!(title_kana_id: title_kana.id)
 
+    #お気に入り登録した作品カナの本を取得
+    to_be_favored_books = RakutenWebService::Books::Book.search(
+      title_kana: title_kana.title_kana,
+      booksGenreId: "001",
+      orFlag: 0,
+      sort: "-releaseDate"
+    )
+
+    # 未発売の本だけfavored_booksテーブルに各種情報を登録
+    favored_books = to_be_favored_books.select{|tob| tob.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}.map do |b|
+      rational_type_days_to_release = Date.parse(b.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01")) - Date.today
+      FavoredBook.new(
+        author_name: author.author_name,
+        isbn: b.isbn,
+        title: b.title,
+        title_kana: b.title_kana,
+        sales_date: b.sales_date,
+        days_to_release: rational_type_days_to_release.to_i,
+        publisher_name: b.publisher_name,
+        item_url: b.item_url,
+        item_price: b.item_price,
+        size: b.size
+      )
+    end
+    FavoredBook.import favored_books, on_duplicate_key_update: {conflict_target: [:isbn]}
+
+    # book_noticesテーブルにcurrent_user.idとfavored_book_idを登録
+    favored_books.each do |b|
+      current_user.notices.create!(favored_book_id: b.id)
+    end
+
     redirect_back(fallback_location: root_path)
   end
 
