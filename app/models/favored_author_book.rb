3718135cbfe2aa41d5959a41832abd17f9b84c0a
Taiji Namba <namba9499@gmail.com>
2020-12-17 21:52:11 +0900

    favored_author_booksとusersの関連付けを修正

--- app/models/favored_author_book.rb
+++ app/models/favored_author_book.rb
@@ -1,6 +1,6 @@
 class FavoredAuthorBook < ApplicationRecord
   has_many :notices, dependent: :destroy
-  has_many :notice_books, through: :notices, source: :users
+  has_many :notice_users, through: :notices, source: :users
   
   validates :isbn, uniqueness:{message: "はすでに登録されています"}
 end
