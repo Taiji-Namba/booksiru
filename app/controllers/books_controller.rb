9e9800ba835c58514b8735afcd6acda4f20e72bd
Taiji Namba <namba9499@gmail.com>
2020-12-18 23:31:33 +0900

    homeアクションで検索される本を設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,14 @@ class BooksController < ApplicationController
   attr_accessor :books 
   
   def home
-    @books = RakutenWebService::Books::Book.search(sort: "sales")
+    books = RakutenWebService::Books::Book.search(
+      sort: "standard",
+      booksGenreId: "001001"
+    )
+    @unreleased_books = books.select{|book| book.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
+      # rational_type_days_to_release = Date.parse(b.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01")) - Date.today rescue 0
+    # end
+    # @unreleased_books = unreleased_books.sort()
   end
 
   def search
