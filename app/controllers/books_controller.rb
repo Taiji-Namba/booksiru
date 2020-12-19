8366414987d98f4af6bb0584142c564e2d5218da
Taiji Namba <namba9499@gmail.com>
2020-12-19 10:29:35 +0900

    homeアクションで検索される本を修正・検索された本を表示するビューを作成(一時保存)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,23 @@ class BooksController < ApplicationController
   attr_accessor :books 
   
   def home
-    books = RakutenWebService::Books::Book.search(
-      sort: "standard",
-      booksGenreId: "001001"
+    comics = RakutenWebService::Books::Book.search(
+      booksGenreId: Genre.find_by(genre: "コミック").booksgenreid,
+      sort: "standard"
     )
-    @unreleased_books = books.select{|book| book.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
-      # rational_type_days_to_release = Date.parse(b.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01")) - Date.today rescue 0
-    # end
-    # @unreleased_books = unreleased_books.sort()
+    @unreleased_comics = comics.select{|book| book.sales_date.delete("/年|月|日|頃| |　|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
+    
+    light_novels = RakutenWebService::Books::Book.search(
+      booksGenreId: Genre.find_by(genre: "ライトノベル").booksgenreid,
+      sort: "standard"
+    )
+    @unreleased_light_novels = light_novels.select{|book| book.sales_date.delete("/年|月|日|頃| |　|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
+
+    novels_and_essays = RakutenWebService::Books::Book.search(
+      booksGenreId: Genre.find_by(genre: "小説・エッセイ").booksgenreid,
+      sort: "standard"
+    )
+    @unreleased_novels_and_essays = novels_and_essays.select{|book| book.sales_date.delete("/年|月|日|頃| |　|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
   end
 
   def search
