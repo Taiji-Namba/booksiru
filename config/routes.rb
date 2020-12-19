ffc3b5f6dafb4737d3a3598657fba6a570ac4860
Taiji Namba <namba9499@gmail.com>
2020-12-19 16:45:02 +0900

    homeをtopに変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     post "users/guest_sign_in", to: "users/sessions#new_guest"
   end
 
-  root "books#home"
+  root "books#top"
 
   resources :users,only: [:favorite] do 
     get "favorite", on: :collection
