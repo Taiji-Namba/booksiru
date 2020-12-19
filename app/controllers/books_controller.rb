77ca983579fd6a4b59c8845b5b712816c5129f7f
Taiji Namba <namba9499@gmail.com>
2020-12-19 11:14:46 +0900

    homeアクションでの未発売の判定式を修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,13 @@ class BooksController < ApplicationController
       booksGenreId: Genre.find_by(genre: "コミック").booksgenreid,
       sort: "standard"
     )
-    @unreleased_comics = comics.select{|book| book.sales_date.delete("/年|月|日|頃| |　|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
+    @unreleased_comics = comics.select{|book| book.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
     
     light_novels = RakutenWebService::Books::Book.search(
       booksGenreId: Genre.find_by(genre: "ライトノベル").booksgenreid,
       sort: "standard"
     )
-    @unreleased_light_novels = light_novels.select{|book| book.sales_date.delete("/年|月|日|頃| |　|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
+    @unreleased_light_novels = light_novels.select{|book| book.sales_date.delete("/年|月|日|頃|/").gsub(/|上旬|中旬|下旬|以降/, "上旬" => "5", "中旬" => "15", "下旬" => "25", "以降" => "01").to_i > Time.current.strftime("%Y%m%d").to_i}
 
     novels_and_essays = RakutenWebService::Books::Book.search(
       booksGenreId: Genre.find_by(genre: "小説・エッセイ").booksgenreid,
