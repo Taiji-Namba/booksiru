51cbfc6ddf5398a5efd34abed47619fa02a12322
Taiji Namba <namba9499@gmail.com>
2020-12-29 20:50:00 +0900

    ゲストユーザーの削除不可設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+            registrations: "users/registrations"
             omniauth_callbacks: "omniauth_callbacks",
           }
   devise_scope :user do 
