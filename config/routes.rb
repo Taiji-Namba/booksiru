ec6237420ab457ece5ab002b4fa8a435751e28bf
Taiji Namba <namba9499@gmail.com>
2020-12-29 21:05:30 +0900

    ゲストユーザーのメールアドレスとパスワードの変更不可設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-            registrations: "users/registrations"
-            omniauth_callbacks: "omniauth_callbacks",
+            passwords: "users/passwords",
+            registrations: "users/registrations",
+            omniauth_callbacks: "omniauth_callbacks"
           }
   devise_scope :user do 
     post "users/guest_sign_in", to: "users/sessions#new_guest"
