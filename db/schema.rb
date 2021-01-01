6414bfd1dc346e44f439cc1d73df67f0086d4789
Taiji Namba <namba9499@gmail.com>
2021-01-01 13:36:02 +0900

    ニックネーム廃止

--- db/schema.rb
+++ db/schema.rb
@@ -123,7 +123,6 @@ ActiveRecord::Schema.define(version: 2020_12_15_114524) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
